42e9c4a06adcd26fa4f3974d719710bffe8939f8
Matheus Gabrie <matheuscoding@gmail.com>
2021-02-12 13:20:19 -0300

    changing names of binarySearchRecursively

--- binarySearch.c
+++ binarySearch.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int binarySearch(int *v,int value,int l,int r)
+int pesquisaBinaria (int *vet, int target, int r)
 {
-    int middle=(l+r)/2;
-
-
-    if(value == v[middle])
-        return middle;
-
-    if(middle == l || middle == r)
-        return -1;
-
-    if(v[middle]>value)
-        return binarySearch(v,value,l,middle);
-    else
-        return binarySearch(v,value,middle,r);
-}
-
-
-int main(void)
-{
-    int a[5] = {1,2,3,4,5};
-
-    printf("%d\n",binarySearch(a,1,0,5));
-
+     int l = 0;     
+     int meio;
+     while (l <= r)
+     {
+          meio = (l + r)/2;
+          if (target == vet[meio])
+               return meio;
+          if (target < vet[meio])
+               r = meio-1;
+          else
+               l = meio+1;
+     }
+     return -1; 
 }
