b1da0b9c458a88d5752339323a960b3175abd4ef
Pedrok <pedrohlima20@gmail.com>
2021-02-21 20:54:08 -0300

    Arrumando busca binaria

--- binarySearch.c
+++ binarySearch.c
@@ -8,12 +8,26 @@ int pesquisaBinaria (int *vet, int target, int r)
      while (l <= r)
      {
           meio = (l + r)/2;
-          if (target == vet[meio])
+          if (target == vet[meio]){
                return meio;
-          if (target < vet[meio])
-               r = meio-1;
-          else
+          }
+              
+          if (vet[meio] < target){
                l = meio+1;
+          }
+               
+          else{
+               r = meio;
+          }
+               
      }
      return -1; 
+}
+
+int main(void)
+{
+    int a[5] = {1,2,3,4,5};
+
+    printf("%d\n", pesquisaBinaria(a, 5, 4));
+
 }
